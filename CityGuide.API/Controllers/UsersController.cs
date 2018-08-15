c3223fc853c055f6e87f0f49f47b59c0aca509ef
mamurev <muratxdincer@hotmail.com>
2018-08-15 14:00:25 +0300

    * Model classes created. * Repository interface and class created.

---
+++ CityGuide.API/Controllers/UsersController.cs
@@ -0,0 +1,16 @@
+﻿using CityGuide.API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityGuide.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        DataContext _context;
+        protected UsersController(DataContext context)
+        {
+            _context = context;
+        }
+    }
+}
--- CityGuide.API/Startup.cs
+++ CityGuide.API/Startup.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CityGuide.API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -24,7 +26,10 @@ namespace CityGuide.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors();
+            services.AddScoped<IAppRepository, AppRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,7 +40,9 @@ namespace CityGuide.API
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
             app.UseMvc();
+            
         }
     }
 }
