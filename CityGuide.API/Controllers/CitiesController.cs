c425c404889806113eb360a9b6f9cbfece3c79cb
mamurev <muratxdincer@hotmail.com>
2018-08-28 00:55:12 +0300

    * AutoMapper added

---
+++ CityGuide.API/Controllers/CitiesController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CityGuide.API.Data;
+using CityGuide.API.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+
+namespace CityGuide.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        private IAppRepository _appRepository;
+        private IMapper _mapper;
+
+        public CitiesController(IAppRepository appRepository, IMapper mapper)
+        {
+            _appRepository = appRepository;
+            _mapper = mapper;
+        }
+
+        public ActionResult GetCities()
+        {
+            ////Auto mapper olmadan DTO yazımında alanların modeldekiyle aynı isimde olması gerekir. 
+            //var cities = _appRepository.GetCities()
+            //                            .Select(c => new CityForListDto { Id = c.Id, Name = c.Name, Description = c.Description, PhotoUrl = c.Photos.FirstOrDefault().Url }).ToList();
+
+            var cities = _appRepository.GetCities();
+            var citiesToReturn = _mapper.Map<List<CityForListDto>>(cities);
+            return Ok(citiesToReturn);
+        }
+    }
+}
--- CityGuide.API/Startup.cs
+++ CityGuide.API/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using CityGuide.API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -30,6 +31,7 @@ namespace CityGuide.API
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddCors();
             services.AddScoped<IAppRepository, AppRepository>();
+            services.AddAutoMapper();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
