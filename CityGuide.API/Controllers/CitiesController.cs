353cc78e5c3ed0a7f5345705b6b09ef4c8bfa784
mamurev <muratxdincer@hotmail.com>
2018-08-28 09:55:30 +0300

    * City post added

--- CityGuide.API/Controllers/CitiesController.cs
+++ CityGuide.API/Controllers/CitiesController.cs
@@ -7,6 +7,7 @@ using CityGuide.API.Dtos;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using AutoMapper;
+using CityGuide.API.Models;
 
 namespace CityGuide.API.Controllers
 {
@@ -32,6 +33,14 @@ namespace CityGuide.API.Controllers
             var cities = _appRepository.GetCities();
             var citiesToReturn = _mapper.Map<List<CityForListDto>>(cities);
             return Ok(citiesToReturn);
+        }
+        [HttpPost]
+        [Route("add")]
+        public ActionResult Add([FromBody]City city)
+        {
+            _appRepository.Add(city);
+            _appRepository.SaveAll();
+            return Ok(city);
         }
     }
 }
